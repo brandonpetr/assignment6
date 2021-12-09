ea3083c9d37c2d892adbb44d254d4fb7c0dadca6
brandonpetr <94504662+brandonpetr@users.noreply.github.com>
2021-12-09 17:28:41 -0500

    Add files via upload

---
+++ Program61.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Assignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            double result = 0.0;
+            try
+            {
+                Console.WriteLine("Enter numbers to divide by ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                
+                num2 = Convert.ToInt32(Console.ReadLine());
+                result = Convert.ToDouble(num1 / (double)num2);
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("{0}", e);
+            }
+            catch (FormatException e1)
+            {
+                Console.WriteLine("Exception caught, you can't do that!!! {0}", e1);
+            }
+            finally
+            {
+                Console.Write(result);
+            }
+        }
+    }
+}
---
+++ Program62.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assignment62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[4];
+            int sum = 0;
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    array[i] = Convert.ToInt32(Console.ReadLine());
+                    sum = sum + array[i];
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Error! {0}", e);
+            }
+            finally
+            {
+                Console.Write(sum);
+            }
+        }
+    }
+}
